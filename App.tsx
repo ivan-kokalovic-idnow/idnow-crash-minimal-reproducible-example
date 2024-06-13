b46cdde938e56cb4ed10740578d0813714f09c29
Svetlana Sapelova <svetlana.sapelova@blocknox.de>
2024-06-13 15:43:40 +0200

    add a text field for the transaction token

--- App.tsx
+++ App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import {SafeAreaView, NativeModules, Button} from 'react-native';
+import {SafeAreaView, NativeModules, Button, TextInput} from 'react-native';
+
+
 
 const RNIdNow = NativeModules.RNIdNow;
 
 function App() {
+  const [transactionToken, setTransactionToken] = React.useState('TST-ZZLHS');
   const settings = {
     companyId: 'solarisbankvideoidentbison',
     environment: 'test',
     showErrorSuccessScreen: false,
     showVideoOverviewCheck: true,
-    transactionToken: 'TST-YMUQE',
+    transactionToken: 'TST-ZZLHS',
   };
 
+
   const startIdNow = async () => {
-    const response = await RNIdNow.start(settings);
-    return {response};
+    try {
+      const response = await RNIdNow.start(settings);
+      return { response };
+    } catch (error: any) {
+      console.error('Error with sdk', error);
+    }
   };
+
+
 
   return (
     <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
+       <TextInput
+        placeholder="Enter Transaction Token"
+        value={transactionToken}
+        onChangeText={setTransactionToken}
+      />
       <Button title={'Start IDNow'} onPress={startIdNow} />
     </SafeAreaView>
   );
